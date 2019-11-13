50d6bb4a85ea28a336e42034eb21917b270906a2
DELL <DELL@JinKyu>
2019-11-13 19:05:04 +0900

    샘플 controller 소스 메인컨트롤러로 이동

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -2,7 +2,11 @@ package com.example.demo;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.java.dogle.mapper.sample.SampleMapper;
 
 /**
  * 
@@ -18,6 +22,25 @@ public class MainController {
 	
 	private static final Logger LOGGER = LogManager.getLogger(MainController.class);
 	
+	
+	@Autowired
+	private SampleMapper sampleMapper;
+	
+	
+	
+	
+	
+	
+	
+	@RequestMapping("/sample1")
+	public String sample1() {
+		
+		int result = sampleMapper.sample1();
+
+		LOGGER.info("sample1 ::: {}", result);
+
+		return "test";
+	}
 	
 	
 	

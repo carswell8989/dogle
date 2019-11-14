3403a0cad070651a055021ebcc9debecf4fc3260
DELL <DELL@JinKyu>
2019-11-14 22:29:55 +0900

    공통VO작성

---
+++ src/main/java/com/java/dogle/common/CommonVO.java
@@ -0,0 +1,13 @@
+package com.java.dogle.common;
+
+
+
+/**
+ * 
+ * VO작성시 해당 VO를 extends 해주시기 바랍니다.
+ *
+ */
+
+public class CommonVO {
+
+}
---
+++ src/main/java/com/java/dogle/common/PageVO.java
@@ -0,0 +1,69 @@
+package com.java.dogle.common;
+
+
+
+/**
+ * 
+ * 페이지 관련 객체 이용시 해당 VO를 extends 해주시기 바랍니다.
+ *
+ */
+
+public class PageVO extends CommonVO {
+	
+	private int pageRow; //한 페이지당 보여줄 글의 수
+ 
+	private int total; //전체 글의 수
+	
+	private int currentPage; //현재 페이지
+	
+	private int offset; //쿼리에서 사용할 offset (현재 페이지에서 보여줄 글의 인덱스)
+	
+	private int lastPage; //마지막 페이지
+
+	public int getPageRow() {
+		return pageRow;
+	}
+
+	public void setPageRow(int pageRow) {
+		this.pageRow = pageRow;
+	}
+
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
+	public int getCurrentPage() {
+		return currentPage;
+	}
+
+	public void setCurrentPage(int currentPage) {
+		this.currentPage = currentPage;
+	}
+
+	public int getOffset() {
+		return offset;
+	}
+
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+
+	public int getLastPage() {
+		return lastPage;
+	}
+
+	public void setLastPage(int lastPage) {
+		this.lastPage = lastPage;
+	}
+	
+	
+	
+	
+	
+	
+	
+}

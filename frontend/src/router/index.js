08e70a85309e5dc6b4e8abac38a6065b872ceb00
yujin_jeon <yujin_jeon@rnbsoft.com>
2019-11-15 16:19:19 +0900

    프론트엔드 - 라우팅 테스트

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/views/HelloWorld'
+import Sample from '@/views/sample1/Sample'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/sample1',
+      name: 'Sample',
+      component: Sample
     }
   ]
 })

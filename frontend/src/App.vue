13b17582f73b3a984a5120445bae71f1c767060d
yujin_jeon <yujin_jeon@rnbsoft.com>
2019-11-18 17:32:14 +0900

    메인화면 네비게이션 element 프레임워크 적용

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,64 @@
 <template>
   <div id="app">
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+    >
+      <el-menu-item index="1">
+        <router-link to="/">홈</router-link>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <router-link to="/hotelSrh">호텔검색</router-link>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <router-link to="/board">공지사항</router-link>
+      </el-menu-item>
+      <el-submenu index="4">
+      <template slot="title"><router-link to="/myPage">마이페이지</router-link></template>
+      <el-menu-item index="2-1">item one</el-menu-item>
+      <el-menu-item index="2-2">item two</el-menu-item>
+      <el-menu-item index="2-3">item three</el-menu-item>
+      <el-submenu index="2-4">
+        <template slot="title">item four</template>
+          <el-menu-item index="2-4-1">item one</el-menu-item>
+          <el-menu-item index="2-4-2">item two</el-menu-item>
+          <el-menu-item index="2-4-3">item three</el-menu-item>
+      </el-submenu>
+      </el-submenu>
+    </el-menu>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      activeIndex: "1"
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
 }
 </script>
 
 <style>
+@import url("//unpkg.com/element-ui@2.12.0/lib/theme-chalk/index.css");
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#app a { display: inline-block; text-decoration: none; width: 100px; font-size: 15px;}
 </style>
--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -2,23 +2,7 @@
   <div class="main">
     <h1>Dogle</h1>
     <h2>Dogle - Main Page</h2>
-    <ul>
-      <li>
-        <a href="./" target="_blank">홈</a>
-      </li>
-      <li>
-        <a href="/hotelSrh" target="_blank">호텔검색</a>
-      </li>
-      <li>
-        <a href="/board" target="_blank">공지사항</a>
-      </li>
-      <li>
-        <a href target="_blank">로그아웃</a>
-      </li>
-      <li>
-        <a href="/myPage" target="_blank">마이페이지</a>
-      </li>
-    </ul>
+    <img src="../assets/logo.png" alt="doggle_logo">
   </div>
 </template>
 

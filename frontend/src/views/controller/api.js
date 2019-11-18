4be3224c5684f34a8ca5e689e0ca2b0c8c43255f
yujin_jeon <yujin_jeon@rnbsoft.com>
2019-11-18 10:55:34 +0900

    Axios 설정추가

--- frontend/src/views/controller/api.js
+++ frontend/src/views/controller/api.js
@@ -2,15 +2,13 @@ import axios from 'axios'
 
 export default {
   hello () {
-    return axios('/hotelSrh', {
+    return axios('http://localhost:9090/hotelSrh', {
       method: 'GET',
       mode: 'no-cors',
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json'
-      },
-      withCredentials: true,
-      credentials: 'same-origin'
+      }
     })
   }
 }

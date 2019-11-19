73fbcac3a1122dbeb84484044fee856cf90d97a3
jinkyu.lee <jinkyu_lee@rnbsoft.com>
2019-11-19 16:58:59 +0900

    로그test - 주석추가.

--- src/main/java/com/example/demo/LogTest.java
+++ src/main/java/com/example/demo/LogTest.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LogTest {
 
-	
+	//로그 필요시 아래 라인 복사해서 클래스를 변경한 뒤 사용해주세요.
 	private static final Logger LOGGER = LogManager.getLogger(LogTest.class);
 	
 	@RequestMapping("/logTest")

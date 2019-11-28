9a2d7047847e4c0045ff9edaecd3082b9f384c48
jinkyu.lee <jinkyu_lee@rnbsoft.com>
2019-11-28 16:56:31 +0900

    불필요한 import 제거

--- src/main/java/com/example/demo/DogleApplication.java
+++ src/main/java/com/example/demo/DogleApplication.java
@@ -1,12 +1,9 @@
 package com.example.demo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
-
 import com.java.dogle.config.FileUploadProperties;
 
 
--- src/main/java/com/java/dogle/controller/MainController.java
+++ src/main/java/com/java/dogle/controller/MainController.java
@@ -1,17 +1,13 @@
 package com.java.dogle.controller;
 
-import java.io.File;
+
 import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.example.demo.DogleException;
 import com.java.dogle.common.FileVO;
 import com.java.dogle.dao.SampleDao;

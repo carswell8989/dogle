1996c2608f71ddd1333a710ac6b4484d6f98bdae
jinkyu.lee <jinkyu_lee@rnbsoft.com>
2019-11-28 16:57:38 +0900

    commonDao 주석처리

--- src/main/java/com/java/dogle/common/CommonDao.java
+++ src/main/java/com/java/dogle/common/CommonDao.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @author jinkyu
  * 
  * 2019.11.19 mapper를 인터페이스를 사용하기 때문에, 해당 클래스 사용하지 않습니다.
+ * 2019.11.28 해당 클래스 재사용. -> mapper 인터페이스 출동문제발생.
  *
  */
 

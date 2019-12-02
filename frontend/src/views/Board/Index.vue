bb9a4ebacbd07e8a88ec5a84e407797de330e3f2
jinkyu.lee <jinkyu_lee@rnbsoft.com>
2019-12-02 09:48:21 +0900

    공지사항 - 테이블 설정(테스트용)

--- frontend/src/views/Board/Index.vue
+++ frontend/src/views/Board/Index.vue
@@ -1,11 +1,18 @@
 <template>
-  <div class="brdList">
-    <h1>공지사항</h1>
-    <h2>공지사항 페이지입니다.</h2>
-  </div>
+  <div>
+    <div class="brdList">
+      <h1>공지사항</h1>
+      <h2>공지사항 페이지입니다.</h2>
+    </div>
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column prop="date" label="Date" width="180"> </el-table-column>
+      <el-table-column prop="name" label="Name" width="180"> </el-table-column>
+      <el-table-column prop="address" label="Address"> </el-table-column>
+    </el-table>
+ </div>
 </template>
-
 <script>
+
 export default {
   name: 'BoardIndex'
 }
